a169cd689079b6cc5f94bd614c18307164196e67
nathaliawebsvg <oliv.natyop@gmail.com>
2025-07-14 17:04:54 -0300

    primeira atividade de logica de python

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+##MVC
+#Model
+###= a entrada dos numeros, modelo ou ordens 
+
+#View
+###= onde vai exibir todos os calculos na tela
+
+
+#Controller
+### vai receber um modelo total e calcular o resultado do view para exiibir na tela
+
+def mostrar_menu():
+    print("/n---calculadora---")
+    print("1. adiçao")
+    print("2. subtraçao")
+    print("5. sair")
+    print("-----------")
+
+    def obter_numeros():
+        while true:
+            
+
+
